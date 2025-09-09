1b964b08db9a61a8f544a4af078ffba111dab27a
Abhinandan-Devadiga <abhinandanabhi17@gmail.com>
2025-09-09 18:10:37 +0530

    Initial Commit

---
+++ main.py
@@ -0,0 +1,17 @@
+import requests
+
+query = input("What type of news you are interested..")
+
+api = "0f2f86f083b745c99d71c36ac2633923"
+
+url = f"https://newsapi.org/v2/everything?q={query}&from=2025-07-03&sortBy=popularity&apiKey={api}"
+
+print(url)
+r = requests.get(url)
+
+data = r.json()
+articles = data["articles"]
+
+for index,article in enumerate(articles):
+    print(index + 1 ,article["title"],article["url"])
+    print("\n********************************************\n")
